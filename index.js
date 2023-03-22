24ee5d7aac07ac86a9325a7dcca69e2583f36f65
Luis Aguilar <elluis.com@live.com>
2023-03-22 13:44:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res) => {
-	res.json({ message: "Hello World"  });
+	res.json({ message: "Hello World "+JSON.stringify(req)  });
+	
 })
 
 app.get("/health-check", (req, res) => {
-	res.json({ message: "Server up and running"  });
+	res.json({ message: "Server up and running"+ JSON.stringify(req)    });
 })
 
 app.listen(PORT, () => {
